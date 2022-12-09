be22b8719b2341bb257050d5dc1a99ca564a7c4e
Yorubaangel <olorunjuwonloadekale@yahoo.com>
2022-12-09 21:06:20 +0100

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+/**
+ * main -Entry point
+ *
+ * Return: always o
+ */
+
+
+ int main(void)
+ {
+	puts("\"Programming is like building a multilingual puzzle")
+
+	return(0);
+ } 
+

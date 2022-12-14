45e459b65d35ee2b397e89bdeac5bbbcd632139e
Yorubaangel <olorunjuwonloadekale@yahoo.com>
2022-12-14 22:33:27 +0100

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _islower - Entry point
+* @c: a character argument
+* Return: returns 1 and 0 depending on condition
+*/
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}

b13d94bccc99e824c7b22289d61ba5edcb46f7ab
Yorubaangel <olorunjuwonloadekale@yahoo.com>
2022-12-15 16:32:42 +0100

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _isupper - check for upper case character.
+* @c: print int
+* Return: returns 1 or 0
+*/
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+

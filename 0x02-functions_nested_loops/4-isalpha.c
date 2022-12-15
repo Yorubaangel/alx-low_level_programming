0c77910f167a3aab731100fb39a728707e40d916
Yorubaangel <olorunjuwonloadekale@yahoo.com>
2022-12-15 02:35:39 +0100

    isalphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _isalpha - checks alphebetical letters
+* @c: a character to be checked on
+* Return: returns 0 and 1 depemding on condition
+*/
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (C >= 'A' && C <= 'Z'));
+}
